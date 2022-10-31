47afbfbe3dcd049c73184d62b163305af26ea6a6
Yuri Ramos <yurigabriel21@yahoo.com>
2022-10-31 03:23:53 -0300

    Implementado o Menu Toogle em dispositivos de menor resolução

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-var n = 10; 
+//**Menu Mobile */
+const menuBtn = document.querySelector('.menu-mobile'); 
+
+function menuToogle(){
+let nav = document.querySelector('.navigation'); 
+nav.classList.toggle('active');
+menuBtn.classList.toggle('active')
+
+}
+
+menuBtn.addEventListener('click', menuToogle);
+
+//**Menu Mobile */
