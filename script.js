cc8fa45cb3ac2b39594bab976f4c6fe27ce0f92f
Yuri Ramos <yurigabriel21@yahoo.com>
2022-10-31 05:18:07 -0300

    implementado comportamento dos links do menu responsivo, apos o clique, o menu se fecha

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
+
+//*Variáveis*/
+
+const menuBtn = document.querySelector('.menu-mobile');
+const nav = document.querySelector('.navigation');
+const itemLink1 = document.querySelector('.ancora1');
+const itemLink2 = document.querySelector('.ancora2');
+const itemLink3 = document.querySelector('.ancora3');
+const itemLink4 = document.querySelector('.ancora4');
+const itemLink5 = document.querySelector('.ancora5');
+
+//*Variáveis*/
+
 //**Menu Mobile */
-const menuBtn = document.querySelector('.menu-mobile'); 
 
 function menuToogle(){
-let nav = document.querySelector('.navigation'); 
 nav.classList.toggle('active');
 menuBtn.classList.toggle('active')
-
 }
-
 menuBtn.addEventListener('click', menuToogle);
 
 //**Menu Mobile */
+
+
+//**Comportamento dos links internos do Menu Toogle */
+
+itemLink1.addEventListener('click', ()=>{
+    menuBtn.classList.remove('active');
+    nav.classList.remove('active');
+})
+    
+itemLink2.addEventListener('click', function(){
+    menuBtn.classList.remove('active');
+    nav.classList.remove('active');
+})
+    
+itemLink3.addEventListener('click', function(){
+    menuBtn.classList.remove('active');
+    nav.classList.remove('active');
+})
+
+itemLink4.addEventListener('click', function(){
+    menuBtn.classList.remove('active');
+    nav.classList.remove('active');
+})
+
+itemLink5.addEventListener('click', function(){
+    menuBtn.classList.remove('active');
+    nav.classList.remove('active');
+})
+
+
+    
+    
+    
+
+//**Comportamento dos links internos do Menu Toogle */
