66a750358d032a3ed808fa60158db385104d811c
Yuri Ramos <yurigabriel21@yahoo.com>
2022-11-04 05:15:26 -0300

    implentação dos comportamentos de scroll, e ajustes na responsividade

--- scroll.js
+++ scroll.js
@@ -6,17 +6,45 @@ ScrollReveal().reveal('.descriptions--sectionOne', {
 }
 ); 
 
-
  /** Animação de revelação */
 
  /** Ocultação dos ícones flutuantes da page */
 
-    if(window.scrollY > 4260){
-        window.alert('chegou!');
+   function FloatIconsNone(){
+    if(window.matchMedia("(max-width:971px)") && window.scrollY >= 3915){
+     document.querySelector('.redes--sociais').style.display ='none';
+    } else{
+     document.querySelector('.redes--sociais').style.display ='flex';     
     }
+   }
 
-
-
-
-
+  addEventListener('scroll', FloatIconsNone);
 /** Ocultação dos ícones flutuantes da page */
+
+
+/** Comportamento do botão Up para subir à pagina principal*/
+
+/**Este bloco de código é responsável pelo click e animação do botão para a home */
+function arrowUp(){
+  window.scrollTo({
+    top:0,
+    behavior: 'smooth'
+  });
+    
+}
+let home = document.querySelector('.btnUp');
+home.addEventListener('click', arrowUp);
+/**Fim do bloco */
+
+
+/**Este bloco de código é responsável por ocultar o botão quando o usuário realiza algum scroll na tela*/
+function ocultarBotaoHome(){
+    if(window.scrollY === 0){
+     document.querySelector('.btn-up').style.display ="none";
+    } else{
+        document.querySelector('.btn-up').style.display ="flex";
+   }
+}
+
+addEventListener('scroll', ocultarBotaoHome);
+/** Fim do bloco */
